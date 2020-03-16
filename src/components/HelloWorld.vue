32fe80c8ad4e020d9e27ff32c98c899f88867f5a
youkaisteve <youkaisteve@gmail.com>
2020-03-16 13:51:31 +0800

    test marker

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,16 +3,17 @@
     <div id="bim" class="left"></div>
     <div class="right">
       <input type="button" v-on:click="getFloors" value="获取楼层" />
+      <input type="button" v-on:click="marker3D" value="3D锚点" />
     </div>
   </div>
 </template>
 
 <script>
 /* eslint-disable no-console */
-import getProvider, { ProviderType } from 'bim-operator';
+import getProvider, { ProviderType } from "bim-operator";
 
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
   },
@@ -21,18 +22,43 @@ export default {
   },
   async mounted() {
     await this.bimface.loadModel({
-      viewToken: '7478da5a8c4a4d259540e3daec0fb45b',
-      url: 'https://static.bimface.com/api/BimfaceSDKLoader/BimfaceSDKLoader@latest-release.js',
-      domId: 'bim'
+      viewToken: "f854308b4b1540f3b2be96337a372544",
+      url:
+        "https://static.bimface.com/api/BimfaceSDKLoader/BimfaceSDKLoader@latest-release.js",
+      domId: "bim"
     });
     // eslint-disable-next-line no-console
-    console.log('finish');
+    console.log("finish");
   },
   methods: {
     async getFloors() {
       const floors = await this.bimface.getFloors();
       console.log(floors);
-      console.log(await this.bimface.getComponentByCondition({ fileId: 1771124467418944, floor: floors[0].name }));
+      console.log(
+        await this.bimface.getComponentByCondition({
+          fileId: 1771874174404448,
+          floor: floors[0].name
+        })
+      );
+    },
+    async marker3D() {
+      const marker3D = {
+        id: 99,
+        src:
+          "http://static.bimface.com/resources/3DMarker/warner/warner_red.png",
+        // size: 100,
+        worldPosition: {
+          x: -9752.023568420416,
+          y: -929.6956396779448,
+          z: 13348.985568386792
+        },
+        tooltip: "这是撒大声地撒旦阿斯顿",
+        onClick: function(item) {
+          console.log(item);
+        }
+      };
+      const id = this.bimface.add3dMarker(marker3D);
+      console.log(`id is ${id}`);
     }
   }
 };
@@ -45,8 +71,8 @@ export default {
   justify-content: flex-start;
 
   .left {
-    width: 800px;
-    height: 600px;
+    width: 1200px;
+    height: 800px;
   }
   .right {
     flex: 1;
